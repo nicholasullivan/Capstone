8e2e6b3886cb5b32c8c7133c6c7ae52da2c7e2b6
nryczek <nikolausfcb13@gmail.com>
2023-03-01 14:03:52 -0600

    formatting

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ class App:
 
         #submit button
         self.submit = tk.Button(self.frame, text="Submit", font= ("lucida 20 bold italic", 8), padx=8, pady=5, command=lambda:[Calculations.matrix_selection(self.var.get()),Calculations.show_tree('./nj.tree')], state = 'disabled')
-        self.submit.place(x= 300,y=400)
+        self.submit.place(x= 350,y=550)
 
         
         
